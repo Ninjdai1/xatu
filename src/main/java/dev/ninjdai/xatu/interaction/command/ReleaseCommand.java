bb7710a56a8285765d08b1496f7df231bf32921d
Ninjdai <ninjdai@duck.com>
2025-05-30 19:18:59 +0200

    feat: show remaining time until merge freezes in /release

--- src/main/java/dev/ninjdai/xatu/interaction/command/ReleaseCommand.java
+++ src/main/java/dev/ninjdai/xatu/interaction/command/ReleaseCommand.java
@@ -56,8 +56,8 @@ public class ReleaseCommand implements Command {
         }
         if (metadata.minor_release_timestamp != null && metadata.minor_release_timestamp > unixTime) {
             reply += "> The next **minor release**, version `%s`, is planned to release <t:%d:R> on <t:%d:f>\n".formatted(currentSemver.nextMinor(), metadata.minor_release_timestamp, metadata.minor_release_timestamp);
-            reply += "> * Big features will not be merged after <t:%d:f>\n".formatted(metadata.minor_release_timestamp - Utils.DAY_LENGTH_IN_SECONDS * 30);
-            reply += "> * Non-bugfixes will not be merged after <t:%d:f>\n".formatted(metadata.minor_release_timestamp - Utils.DAY_LENGTH_IN_SECONDS * 14);
+            reply += "> * Big features will not be merged after <t:%d:f> (<t:%d:R>)\n".formatted(metadata.minor_release_timestamp - Utils.DAY_LENGTH_IN_SECONDS * 30, metadata.minor_release_timestamp - Utils.DAY_LENGTH_IN_SECONDS * 30);
+            reply += "> * Non-bugfixes will not be merged after <t:%d:f> (<t:%d:R>)\n".formatted(metadata.minor_release_timestamp - Utils.DAY_LENGTH_IN_SECONDS * 14, metadata.minor_release_timestamp - Utils.DAY_LENGTH_IN_SECONDS * 14);
         }
         InteractionApplicationCommandCallbackReplyMono replyMono = event.reply(reply);
         return replyMono.withEphemeral(ephemeralOption.isEmpty() || ephemeralOption.get().getValue().get().asBoolean());
